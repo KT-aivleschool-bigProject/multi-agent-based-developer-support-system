dc6b008a6be43f42743274f4c99b50287c4bfcfc
yangminseok <yms090412@naver.com>
2025-08-04 14:43:40 +0900

    projectmanagement 추가

---
+++ projectmanagement/src/main/java/multiagentbaseddevelopersupportsystem/domain/CreateProjectCommand.java
@@ -0,0 +1,14 @@
+package multiagentbaseddevelopersupportsystem.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import lombok.Data;
+
+@Data
+public class CreateProjectCommand {
+
+    private String projectName;
+    private String projectDescription;
+    private List<String> attachments;
+    private ProjectStatus projectStatus;
+}
