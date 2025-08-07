7d510f4d085e7f9d353f3f5d7407c0feecb14242
yunprac <yunprac@gmail.com>
2025-08-07 15:32:11 +0900

    frontend 수정정

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
+
 export default {
 	darkMode: ["class"],
 	content: [
@@ -92,4 +93,4 @@ export default {
 		}
 	},
 	plugins: [require("tailwindcss-animate")],
-}; 
+} satisfies Config;
