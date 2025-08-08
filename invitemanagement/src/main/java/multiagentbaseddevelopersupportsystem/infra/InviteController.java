b266c9f23c7799e9c3c37c7f803bc222e30e591b
hoon0919 <dhtmdgns0919@gmail.com>
2025-08-08 01:03:42 +0000

    invite 수정

--- invitemanagement/src/main/java/multiagentbaseddevelopersupportsystem/infra/InviteController.java
+++ invitemanagement/src/main/java/multiagentbaseddevelopersupportsystem/infra/InviteController.java
@@ -1,11 +1,11 @@
-package multiagentsystem.infra;
+package multiagentbaseddevelopersupportsystem.infra;
 
 import java.util.List; 
 import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
-import multiagentsystem.domain.*;
+import multiagentbaseddevelopersupportsystem.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
