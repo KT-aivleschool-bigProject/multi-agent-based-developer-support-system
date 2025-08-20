680c25a5df3313cfeee68878d820ce53851a2e79
Heeji Jang <heeji0502@gmail.com>
2025-08-20 05:07:44 +0000

    feat: 게시판 첨부파일 참고하여 제목과 글 추천 기능 추가

---
+++ attachment/src/main/java/multiagentbaseddevelopersupportsystem/domain/SwaggerYamlPost.java
@@ -0,0 +1,38 @@
+package multiagentbaseddevelopersupportsystem.domain;
+
+import java.util.Date;
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import multiagentbaseddevelopersupportsystem.AttachmentApplication;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "swagger_yaml_posts")
+public class SwaggerYamlPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long syId;
+
+    @Lob
+    @Column(name = "yaml_content", nullable = false)
+    private String yamlContent;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
---
+++ attachment/src/main/java/multiagentbaseddevelopersupportsystem/domain/SwaggerYamlPostRepository.java
@@ -0,0 +1,8 @@
+package multiagentbaseddevelopersupportsystem.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SwaggerYamlPostRepository extends JpaRepository<SwaggerYamlPost, Long> {
+}
