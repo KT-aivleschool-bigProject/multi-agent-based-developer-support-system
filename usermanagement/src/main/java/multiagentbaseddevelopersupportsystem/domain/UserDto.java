e32fc1f6b2fc2b59e2568cd31e1a16eeb0d7be76
Heeji Jang <heeji0502@gmail.com>
2025-08-22 01:19:35 +0000

    feat: 개인정보 마스킹 기능 추가

--- usermanagement/src/main/java/multiagentbaseddevelopersupportsystem/domain/UserDto.java
+++ usermanagement/src/main/java/multiagentbaseddevelopersupportsystem/domain/UserDto.java
@@ -1,14 +1,19 @@
 package multiagentbaseddevelopersupportsystem.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import multiagentbaseddevelopersupportsystem.config.MaskingSerializer;
 
 @Data
 @Builder
 @AllArgsConstructor
 public class UserDto {
+    @JsonSerialize(using = MaskingSerializer.NameSerializer.class)
     private String name;
+    @JsonSerialize(using = MaskingSerializer.EmailSerializer.class)
     private String email;
     private String position;
 }
