2f08cf727236db2a4370472738bbf9cefe3e1fb2
Heeji Jang <heeji0502@gmail.com>
2025-08-22 01:19:35 +0000

    feat: 개인정보 마스킹 기능 추가

--- usermanagement/src/main/java/multiagentbaseddevelopersupportsystem/infra/UserController.java
+++ usermanagement/src/main/java/multiagentbaseddevelopersupportsystem/infra/UserController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import antlr.Token;
 import lombok.RequiredArgsConstructor;
 import multiagentbaseddevelopersupportsystem.domain.UserDto;
 import multiagentbaseddevelopersupportsystem.security.TokenProvider;
