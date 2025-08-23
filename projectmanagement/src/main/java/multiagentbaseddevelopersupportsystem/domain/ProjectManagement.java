a97a98318b3b3de398cfd4e3d0cdf38d890b790e
Heeji Jang <heeji0502@gmail.com>
2025-08-23 07:21:32 +0000

    fix: projectMangement 프로젝트 생성 로직 변경

--- projectmanagement/src/main/java/multiagentbaseddevelopersupportsystem/domain/ProjectManagement.java
+++ projectmanagement/src/main/java/multiagentbaseddevelopersupportsystem/domain/ProjectManagement.java
@@ -33,25 +33,6 @@ public class ProjectManagement {
         return projectManagementRepository;
     }
 
-    //<<< Clean Arch / Port Method
-    //프로젝트 생성 및 초대 처리
-    public void createProject(CreateProjectCommand createProjectCommand) {
-        // 프로젝트 정보 설정
-        this.projectName = createProjectCommand.getProjectName();
-        this.projectDescription = createProjectCommand.getProjectDescription();
-        this.githubUrl = createProjectCommand.getGithubUrl();
-        this.projectStatus = createProjectCommand.getProjectStatus();
-
-        // 프로젝트 생성 이벤트 발행
-        ProjectCreated projectCreated = new ProjectCreated(this);
-        projectCreated.publishAfterCommit();
-
-        // 초대 이메일이 있다면 초대 이벤트 발행
-        if (createProjectCommand.getInviteEmails() != null && !createProjectCommand.getInviteEmails().isEmpty()) {
-            inviteMembers(createProjectCommand.getInviteEmails());
-        }
-    }
-
     //>>> Clean Arch / Port Method
     //이메일 기반 팀원 초대 이벤트 발행
     public void inviteMembers(List<String> emails) {
