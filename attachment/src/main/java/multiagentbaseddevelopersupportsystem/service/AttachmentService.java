7c5d196d008e61864d7ea979517def45903c1eda
heeji <heeji0502@gmail.com>
2025-08-28 14:12:07 +0900

    chore: 문서화 에이전트 배포 주소 삽입

--- attachment/src/main/java/multiagentbaseddevelopersupportsystem/service/AttachmentService.java
+++ attachment/src/main/java/multiagentbaseddevelopersupportsystem/service/AttachmentService.java
@@ -312,7 +312,7 @@ public class AttachmentService {
     }
 
     private DocAgentResponse analyzeAttachmentAndSaveSwagger(ProjectAttachmentRequest fileInfo) {
-        String fastApiUrl = "https://2c386dc6931c.ngrok-free.app/analyze"; // 실제 엔드포인트로 변경
+        String fastApiUrl = "http://doc-agent-backend-service.default.svc.cluster.local/analyze"; // 실제 엔드포인트로 변경
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
@@ -402,7 +402,7 @@ public class AttachmentService {
     }
 
     public SwaggerYamlPostResponse updateSwaggerYaml(SwaggerYamlPostRequest request) {
-        String fastApiUrl = "https://2c386dc6931c.ngrok-free.app/finalize-yaml"; // 실제 엔드포인트로 변경
+        String fastApiUrl = "http://doc-agent-backend-service.default.svc.cluster.local/finalize-yaml"; // 실제 엔드포인트로 변경
         RestTemplate restTemplate = new RestTemplate();
 
         HttpHeaders headers = new HttpHeaders();
